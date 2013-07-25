4b0f8caecda0a06472e4c07fe50142dd41a41148
hiloki <service@inkdesign.jp>
2013-07-26 04:24:35 +0900

    scaffold

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+/*jshint node:true*/
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		webfont: {
+			icons: {
+				src: 'svg/*.svg',
+				dest: 'symbol',
+				destCss: 'css',
+				options: {
+					font: 'symbol',
+					template: 'templates/template.css',
+					htmlDemo: false,
+					stylesheet: 'scss',
+					// stylesheet: 'less',
+					// stylesheet: 'stylus',
+					relativeFontPath: '../symbol/'
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-webfont');
+
+	grunt.registerTask('default', ['webfont']);
+	grunt.registerTask('build', ['default']);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-symbols
-=======
+# Symbol icons
+
+## Requirement
+
+- [Grunt.js 0.4](http://gruntjs.com/)
+- [grunt-webfont and depending](https://github.com/sapegin/grunt-webfont)
+- 
+
+## Credit
+
+- [Original 'Symbols.ai'](https://github.com/cognitom/symbols)
